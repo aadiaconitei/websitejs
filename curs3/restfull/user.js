ae821a418d3e46d9b37279519106429a1f51092a
miezuit <miezuit@gmail.com>
2022-04-05 17:19:25 +0300

    curs4

--- curs3/restfull/user.js
+++ curs3/restfull/user.js
@@ -4,7 +4,7 @@
 
 // Get all users
 function httpGet(){
-var url  = "http://localhost/javascript/curs5/restfull/users.php";
+var url  = "http://localhost/mysite/users.php";
 var xhr  = new XMLHttpRequest();
 xhr.open('GET', url, true)
 //xhr.readystatechange = function () {
@@ -20,19 +20,19 @@ xhr.onload = function () {
 xhr.send(null);
 }
 
-//Get a user
-var url  = "http://localhost/javascript/curs5/restfull/users.php";
-var xhr  = new XMLHttpRequest()
-xhr.open('GET', url+'?id=2', true)
-xhr.onload = function () {
-	var users = JSON.parse(xhr.responseText);
-	if (xhr.readyState == 4 && xhr.status == "200") {
-		console.table(users);
-	} else {
-		console.error(users);
-	}
-}
-xhr.send(null);
+// //Get a user
+// var url  = "http://localhost/javascript/curs5/restfull/users.php";
+// var xhr  = new XMLHttpRequest()
+// xhr.open('GET', url+'?id=2', true)
+// xhr.onload = function () {
+// 	var users = JSON.parse(xhr.responseText);
+// 	if (xhr.readyState == 4 && xhr.status == "200") {
+// 		console.table(users);
+// 	} else {
+// 		console.error(users);
+// 	}
+// }
+// xhr.send(null);
 
 
 // Post a user
